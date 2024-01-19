c8fdfe0d90ed6d1688124f6569a788dcad399d2d
PatrickLubbers <134800717+PatrickLubbers@users.noreply.github.com>
2024-01-19 15:54:59 +0100

    Update username.php
    Added extra registration form

--- username.php
+++ username.php
@@ -1,12 +1,13 @@
 <?php
 
 session_start();
+
 include 'functions.php';
 
-$connection = connect_to_database();
-
 show_form();
-check_login($connection);
+//check_login($connection);
+
+//presentation layer
 
 function show_form() {
 	?>
@@ -18,13 +19,26 @@ function show_form() {
 </head>
 <body>
 
+<h4>Login form</h4>
 	<form method="post">
 		<label for="user">Enter username:</label>
 		<input type="text" id="user" name="user"> <!--ID is used for javascript and css styling. name is used for form submission -->
 		<label for="password">Enter password:</label>
 		<input type="password" id="password" name="password">
-		<button type="submit">Submit login</button>
+		<button type="submit" name="login_submit">Submit login</button>
 	</form>
+	
+		<!--Create registration form-->
+		<!--All checks and authentication should be run from the input data -->
+<h4>Registration form</h4>
+	<form method="post">
+		<label for="user">Enter username:</label>
+		<input type="text" id="user" name="register_user"> <!--ID is used for javascript and css styling. name is used for form submission -->
+		<label for="password">Enter password:</label>
+		<input type="password" id="password" name="register_password">
+		<button type="submit" name="register_submit">Submit registration</button>
+	</form>
+		 
 </body>
 </html>
 	
