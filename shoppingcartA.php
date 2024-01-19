3b410a3e81db389877dbb1c2752d506c8c20497a
PatrickLubbers <134800717+PatrickLubbers@users.noreply.github.com>
2024-01-19 15:53:48 +0100

    Update shoppingcartA.php
    No big changes here, just changed argument when calling show_shopping_page() function

--- shoppingcartA.php
+++ shoppingcartA.php
@@ -4,6 +4,7 @@ session_start();
 include 'functions.php';
 
 $user = $_SESSION['user'];
+$items = get_items($connection);
 
 echo "welcome $user!";
 echo "<br><br>";
@@ -19,11 +20,15 @@ if (isset($_POST['addToCart'])) {
     insert_into_cart($connection, $itemId, $user, $userId); //inserts into database
 }
 
-show_shopping_page($connection, $result); //retrieves data from database
+show_shopping_page($items); //retrieves data from database
 show_what_is_in_cart($connection, $user, $userId); //retrieves data from database
 
 mysqli_close($connection);
 
+
+//the errors indeed came from some refactoring efforts renaming variables $dbConnection to $connection
+//but in this file $connection was not defined yet. What is better, defining this variable in here 
+//or in the functions table? In function.php would enable me to cut out 1 line of code from both shoppingcartA.php and username.php
 
 //the errors indeed came from some refactoring efforts renaming variables $dbConnection to $connection
 //but in this file $connection was not defined yet. What is better, defining this variable in here 
