696214e1acc017ccdc370a7cf5afdf04d2349822
patrick lubbers <rosevalley@gmail.com>
2024-01-22 20:40:43 +0100

    #made home and contact pages, added amount column in database, allow user to increase this amount before ordering

---
+++ page_contact.php
@@ -0,0 +1,37 @@
+<?php
+
+session_start();
+
+include 'datalayer.php';
+include 'businesslayer.php';
+include 'presentationlayer.php';
+include 'styling.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Contact page</title>
+</head>
+
+<body>
+    <header>
+
+    </header>
+
+    <section class="welcome-section">
+        <div class="container">
+            <h1>Contact details</h1>
+            <p>Email: Rosevalley@gmail.com</p>
+			<br>
+			<p>To buy products, firstly login</p>
+        </div>
+    </section>
+
+    <footer>
+
+    </footer>
+</body>
+
+</html>
---
+++ page_home.php
@@ -0,0 +1,37 @@
+<?php
+
+session_start();
+
+include 'datalayer.php';
+include 'businesslayer.php';
+include 'presentationlayer.php';
+include 'styling.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Homepage</title>
+</head>
+
+<body>
+    <header>
+
+    </header>
+
+    <section class="welcome-section">
+        <div class="container">
+            <h1>Welcome to this webshop</h1>
+            <p>Any kind of product can be found under 'Browse shop'</p>
+			<br>
+			<p>To buy products, firstly login</p>
+        </div>
+    </section>
+
+    <footer>
+
+    </footer>
+</body>
+
+</html>
